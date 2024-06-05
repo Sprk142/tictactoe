0552342132be9aaadef228f8e7957065440b7ca9
Sprk142 <stormalongjohn722@gmail.com>
2024-06-05 23:25:13 +0100

    Update App.js
    - Updated the Board function to contain state handling, this now dynamically updates the array once a square is clicked
    
    - Updated the handleClick function to no longer allow the upper left square to be clicked, any square can now render "X"
    
    - Added comments to explain the logical process

--- App.js
+++ App.js
@@ -1,28 +1,43 @@
 import { useState } from "react";
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square({ value, onSquareClick }) {
+  //This is a rendering of a single square.
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  //This is the board component, it maintains the state of the game.
+
+  function handleClick(i) {
+    const nextSquares = squares.slice(); // Create a copy of the current state
+    nextSquares[i] = "X"; // Update the clicked square
+    setSquares(nextSquares); // Update the state with the new array
+  }
+  //This function is called when a square it clicked.
+
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
+    //This component renders the 3x3 grid. Each value is recorded by the array above.
   );
 }
